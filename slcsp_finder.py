7aa15bfd7bfbd311541dad6791b67e5d725c6ca8
anthony-barricelli <anthonyjbarricelli@gmail.com>
2024-01-19 19:47:52 -0500

    refactor for modularity/testing

--- slcsp_finder.py
+++ slcsp_finder.py
@@ -3,6 +3,39 @@ import os
 import pandas as pd
 
 
+def filter_silver_plans(df):
+    """
+    Filter silver plans from a DataFrame.
+
+    :param df: DataFrame containing plan information.
+    :return: DataFrame containing only Silver plans.
+    """
+    return df[df['metal_level'] == 'Silver']
+
+
+def get_rate_area_to_slcsp(df):
+    """
+    Returns a DataFrame with the second least cost rate for each state and rate area.
+
+    :param df: A pandas DataFrame containing the rates for each state and rate area.
+    :return: A pandas DataFrame with the second least rate for each state and rate area.
+    """
+    return df.groupby(['state', 'rate_area'])['rate'].apply(
+        lambda x: sorted(x.unique())[1] if len(x) >= 2 else ''
+    )
+
+def get_zipcode_to_rate_area(df):
+    """
+    Filters the given DataFrame to include only zipcodes that have a single state and a single rate area.
+
+    :param df: The DataFrame containing the data.
+    :return: A new DataFrame with only the zipcodes that have a single state and a single rate area.
+    """
+    df = df.groupby('zipcode').filter(
+        lambda x: len(x['state'].unique()) == 1 and len(x['rate_area'].unique()) == 1
+    )
+    return df.groupby('zipcode').agg({'state': 'first', 'rate_area': 'first'})
+
 def find_slcsp():
     script_dir = os.path.dirname(__file__)
 
@@ -11,23 +44,18 @@ def find_slcsp():
     slcsp = pd.read_csv(os.path.join(script_dir, 'data', 'slcsp.csv'), dtype={'zipcode': str})
 
     # Filter out non-silver metal level plans
-    plans = plans[plans['metal_level'] == 'Silver']
+    plans = filter_silver_plans(plans)
 
     # Group plans on state rate area, and store second-highest cost if >= 2 rates, else store ''
-    plans = plans.groupby(['state', 'rate_area'])['rate'].apply(
-        lambda x: sorted(x.unique())[1] if len(x) >= 2 else ''
-    )
+    plans = get_rate_area_to_slcsp(plans)
 
-    #print(plans)
+    print(plans)
 
     # Filter all zipcodes based on whether they are
     #   1. in multiple rate areas
     #   2. in multiple states (thus multiple rate areas)
     # Either of these conditions make SLCSP indeterminate
-    zips = zips.groupby('zipcode').filter(
-        lambda x: len(x['state'].unique()) == 1 and len(x['rate_area'].unique()) == 1
-    )
-    zips = zips.groupby('zipcode').agg({'state': 'first', 'rate_area': 'first'})
+    zips = get_zipcode_to_rate_area(zips)
 
     print(zips)
 
