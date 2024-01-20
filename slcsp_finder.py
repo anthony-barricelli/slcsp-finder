57434f75956e1ca2e8c3549695018e10818f561f
anthony-barricelli <anthonyjbarricelli@gmail.com>
2024-01-19 19:15:21 -0500

    filter out non-silver metals from our plans DF

--- slcsp_finder.py
+++ slcsp_finder.py
@@ -1,6 +1,8 @@
 import os
 
 import pandas as pd
+
+
 def find_slcsp():
     script_dir = os.path.dirname(__file__)
 
@@ -8,9 +10,11 @@ def find_slcsp():
     zips = pd.read_csv(os.path.join(script_dir, 'data', 'zips.csv'), dtype={'zipcode': str})
     slcsp = pd.read_csv(os.path.join(script_dir, 'data', 'slcsp.csv'), dtype={'zipcode': str})
 
+    # Filter out non-silver metal level plans
+    plans = plans[plans['metal_level'] == 'Silver']
+
     print(plans)
-    print(zips)
-    print(slcsp)
+
 
 if __name__ == '__main__':
     find_slcsp()
