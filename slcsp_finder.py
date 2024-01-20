6baf88a27149a861042a83148fa08bfe1f0bbe7e
anthony-barricelli <anthonyjbarricelli@gmail.com>
2024-01-19 19:11:30 -0500

    load csvs into pandas DFs

--- slcsp_finder.py
+++ slcsp_finder.py
@@ -1,6 +1,16 @@
-def find_slcsp():
-    pass
+import os
 
+import pandas as pd
+def find_slcsp():
+    script_dir = os.path.dirname(__file__)
+
+    plans = pd.read_csv(os.path.join(script_dir, 'data', 'plans.csv'))
+    zips = pd.read_csv(os.path.join(script_dir, 'data', 'zips.csv'), dtype={'zipcode': str})
+    slcsp = pd.read_csv(os.path.join(script_dir, 'data', 'slcsp.csv'), dtype={'zipcode': str})
+
+    print(plans)
+    print(zips)
+    print(slcsp)
 
 if __name__ == '__main__':
     find_slcsp()
