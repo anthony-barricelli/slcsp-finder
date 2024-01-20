2d6a08ba701b9f32c2a93fe0be64ee3ed1a64c05
anthony-barricelli <anthonyjbarricelli@gmail.com>
2024-01-19 19:21:13 -0500

    find slcsp per state rate area

--- slcsp_finder.py
+++ slcsp_finder.py
@@ -13,6 +13,11 @@ def find_slcsp():
     # Filter out non-silver metal level plans
     plans = plans[plans['metal_level'] == 'Silver']
 
+    # Group plans on state rate area, and store second-highest cost if >= 2 rates, else store ''
+    plans = plans.groupby(['state', 'rate_area'])['rate'].apply(
+        lambda x: sorted(x.unique())[1] if len(x) >= 2 else ''
+    )
+
     print(plans)
 
 
